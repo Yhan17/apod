8028549188ee4058d1b4459239142af93dab11c0
Yhan17 <yhannunesleal@gmail.com>
2025-01-19 20:46:47 -0300

    Implement remove APOD functionality with datasource, repository, and use case

--- lib/app/modules/favorites_details/favorite_details_factory.dart
+++ lib/app/modules/favorites_details/favorite_details_factory.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../core/entities/apod_entity.dart';
 import 'data/favorite_details_data.dart';
+import 'domain/favorite_details_domain.dart';
 import 'presentation/favorite_details_presentation.dart';
 
 class FavoritesDetailsFactory {
@@ -21,9 +22,14 @@ class FavoritesDetailsFactory {
   static FavoriteDetailViewModel _buildViewModel(ApodEntity apod) {
     final service = SaveApodInHomeServiceImpl();
 
+    final datasource = RemoveApodDatasourceImpl();
+    final repository = RemoveApodRepositoryImpl(datasource);
+    final usecase = RemoveApodUseCase(repository);
+
     return FavoriteDetailViewModel(
       apod: apod,
       saveApodInHomeService: service,
+      removeApodUseCase: usecase,
     );
   }
 
--- lib/app/modules/favorites_details/presentation/widgets/details_bottom_action_widget.dart
+++ lib/app/modules/favorites_details/presentation/widgets/details_bottom_action_widget.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class DetailsBottomActionWidget extends StatelessWidget {
   final VoidCallback onTransformWidget;
+  final VoidCallback onUnfavorite;
 
   const DetailsBottomActionWidget({
     super.key,
     required this.onTransformWidget,
+    required this.onUnfavorite,
   });
 
   @override
@@ -17,10 +19,10 @@ class DetailsBottomActionWidget extends StatelessWidget {
         ),
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         child: Row(
+          spacing: 4,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Expanded(
-              flex: 4,
               child: InkWell(
                 onTap: onTransformWidget,
                 borderRadius: BorderRadius.circular(8),
@@ -34,6 +36,26 @@ class DetailsBottomActionWidget extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(vertical: 12),
                     child: const Text(
                       'Transformar em Widget',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: InkWell(
+                onTap: onUnfavorite,
+                borderRadius: BorderRadius.circular(8),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: const Text(
+                      'Desfavoritar',
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
